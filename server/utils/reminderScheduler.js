89dec5a04ef6c2318f21032ab1b44870b1fdf368
jotishnitr <djotishkumar.2020@gmail.com>
2026-07-21 19:55:57 +0530

    updated time system

--- server/utils/reminderScheduler.js
+++ server/utils/reminderScheduler.js
@@ -16,7 +16,6 @@ cron.schedule("* * * * *", async () => {
       timeZone: "Asia/Kolkata",
       hour: "2-digit",
       minute: "2-digit",
-      second: "2-digit",
       hour12: false,
     });
 
