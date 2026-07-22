71bc263a5f8a0ffd8e2859ffc9c6ae6f8fa896d7
jotishnitr <djotishkumar.2020@gmail.com>
2026-07-22 11:27:03 +0530

    Updating the medicine reset status

--- server/controllers/getMedicine.js
+++ server/controllers/getMedicine.js
@@ -2,8 +2,29 @@ const Medicine = require("../models/Medicine");
 
 const getMedicine = async (req, res) => {
   try {
+    const userId = req.user.id;
+
+    // Get current date normalized to start of today in local/UTC server time
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    // Update medicines where status is true and takenDate was before today
+    await Medicine.updateMany(
+      {
+        userId,
+        status: true,
+        takenDate: { $lt: today }
+      },
+      {
+        $set: {
+          status: false,
+          takenDate: null
+        }
+      }
+    );
+
     const medicines = await Medicine.find({
-      userId: req.user.id
+      userId
     });
 
     res.status(200).json(medicines);
