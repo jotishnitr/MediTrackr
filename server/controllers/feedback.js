5a4a51fec9bb74a938e94e33fbf802f40341b379
Jotish <jotish@gmail.com>
2026-07-30 17:01:01 +0530

    fixed typo in controller in feedback form

--- server/controllers/feedback.js
+++ server/controllers/feedback.js
@@ -1,28 +1,26 @@
-const Feedback = require('../models/Feedback');
+const Feedback = require("../models/Feedback");
 
-const Feedback = async (req, res) => {
-    try {
-        const { rating, category, message } = req.body;
-        const feedback = await Feedback.create({
-            userId: req.user.id,
-            rating,
-            category,
-            message,
-        })
-        await feedback.save();
-        res.status(200).json({
-            success: true,
-            message: "Feedback is stored successfully"
-        })
-
-    }
-    catch (err) {
-        res.status(404).json({
-            success: false,
-            message: "Feedback is not stored successfully"
-        })
-        console.err(err.message);
-    }
-}
+const feedback = async (req, res) => {
+  try {
+    const { rating, category, message } = req.body;
+    const feedback = await Feedback.create({
+      userId: req.user.id,
+      rating,
+      category,
+      message,
+    });
+    await feedback.save();
+    res.status(200).json({
+      success: true,
+      message: "Feedback is stored successfully",
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "Feedback is not stored successfully",
+    });
+    console.err(err.message);
+  }
+};
 
 module.exports = feedback;
